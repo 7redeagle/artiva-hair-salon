eb9a1091a6f08bba37406405dc4bdf2fea396851
tiger-fish <25111698+tiger-fish@users.noreply.github.com>
2020-11-09 18:24:15 -0500

    added active menu js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 $(document).ready(function () {
   $('.hero').height($(window).height());
 })
+
+// active menu item 
+let activeNavItem = $('.nav-item');
+activeNavItem.click(function () {
+  activeNavItem.removeClass('active');
+  $(this).addClass('active');
+});
